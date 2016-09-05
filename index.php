c65f229507f13638ef4376393ad9bbdcf48b3aea
if15 <if15,DTI>
2016-09-05 17:11:07 +0300

    Jaotasin eraldi failidesse

--- index.php
+++ index.php
@@ -1,13 +1,5 @@
 <?php
-  class Hoidla {
-    private $arvud = array();
-    public function lisa($arv) {
-      array_push($this->arvud, $arv);
-    }
-    public function kogus() {
-      return count($this->arvud);
-    }
-  }
+  require 'hoidla.php';
   $vanused = new Hoidla();
   $vanused->lisa(25);
   $vanused->lisa(15);
