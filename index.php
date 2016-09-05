423af06cc112c848861dd582fca4f3af4eac0750
if15 <if15,DTI>
2016-09-05 17:14:03 +0300

    Bugfix

--- index.php
+++ index.php
@@ -1,7 +1,17 @@
 <?php
-  require 'hoidla.php';
+  require("hoidla.php");
   $vanused = new Hoidla();
   $vanused->lisa(25);
   $vanused->lisa(15);
-  echo $vanused->kogus();
 ?>
+<!DOCTYPE html>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Test</title>
+  </head>
+  <body>
+    <?php echo $vanused->count(); ?>
+  </body>
+</html>
